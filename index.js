4b0dff3b9817336c1cd97e8c7ec4aacb4bce771a
Feng <yunfengzhang789@gmail.com>
2023-10-07 05:50:01 -0400

    added basic js open gh link, and basic css to projects

---
+++ index.js
@@ -0,0 +1,20 @@
+const graphEasy = document.getElementById("grapheasy");
+const atomSimulation = document.getElementById("atom-simulation");
+const personalWebsite = document.getElementById("personal-website");
+
+// EVENT LISTENER FOR GRAPHEASY
+graphEasy.addEventListener("click", () => {
+    window.open("https://github.com/fengzhang789/GraphEasy");
+})
+
+
+// EVENT LISTENER FOR ATOM SIMULATION
+atomSimulation.addEventListener("click", () => {
+    window.open("https://github.com/fengzhang789/Atom-Simulation");
+})
+
+
+// EVENT LISTENER FOR PERSONAL WEBSITE
+personalWebsite.addEventListener("click", () => {
+    window.open("https://github.com/fengzhang789/Personal-Website");
+})
