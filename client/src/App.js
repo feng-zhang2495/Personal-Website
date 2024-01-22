c23a81a2a84a3c3ef940a1a4f752a4e99385964b
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-22 04:09:15 -0500

    added more to hero and added animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import { createContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import feng from '../assets/img/feng.JPG'
 
 const Hero = () => {
     return (
         <main className='hero h-screen w-screen'>
-            <div className='flex flex-row'>
-                <div className='left-hero basis-50 pt-44 px-44'>
-                    <h1 className='gradient-text text-7xl'>Feng Zhang</h1>
+            <div className='flex flex-row pt-44 px-32 justify-between'>
+                <div className='left-hero basis-3/6 flex flex-col items-start'>
+                    <h1 className='gradient-text'>Feng Zhang</h1>
+                    <h3 className='typewriter pt-8'>Software Engineer</h3>
+                    <h5 className='text-lg pt-4'>Hello, I'm a first year CS student at the University of Waterloo. I'm deeply passionate in all fields surrounding web development and Machine Learning, and I am constantly looking for new opportunities to improve my skills!</h5>
                 </div>
-                <div className='right-hero basis-50'></div>
+                <div className='right-hero basis-2/6 flex justify-center items-center'>
+                    <img src={feng} alt='feng headshot at e7' className='w-fit h-fit aspect-square max-h-80 rounded-full object-cover'></img>
+                </div>
             </div>
         </main>
     )
