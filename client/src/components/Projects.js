f21f5144585604585fe5b04b3473640fa4647016
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-22 04:31:03 -0500

    added projects section template

---
+++ client/src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <section className='projects w-screen h-screen bg-slate'>
+
+        </section>
+    )
+}
+
+export default Projects
